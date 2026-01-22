e0220f42c5047c3c47b11479a5006ab118323409
Šimon Pavera <simonpavera@seznam.cz>
2026-01-22 10:28:53 +0100

    poznamky
    Updated Player class with comments in Czech for clarity.

--- classes/player.js
+++ classes/player.js
@@ -1,41 +1,42 @@
 class Player {
   constructor(x, groundY, size) {
-    this.x = x;
-    this.size = size;
+    this.x = x; /*nastaveni x-ove souradnice */
+    this.size = size; /*nastaveni velikosti hrace*/
 
-    this.groundY = groundY;
-    this.y = groundY;
+    this.groundY = groundY; /*Nastaví hráče na zem*/
+    this.y = groundY; 
 
-    this.vy = 0;
-    this.gravity = 1.05;
-    this.jumpForce = -15;
+    this.vy = 0; /*rychlost nahoru a dolů*/
+    this.gravity = 1.05; /*gravitace*/
+    this.jumpForce = -15; /*Síla skoku (záporná = nahoru)*/
 
     this.onGround = true;
   }
 
-  jump() {
-    if (this.onGround) {
-      this.vy = this.jumpForce;
-      this.onGround = false;
+  jump() {/*může skočit jen když je na zemi.*/
+    if (this.onGround) {/*hrac bude moct skocit jen kdyz je na zemi*/
+      this.vy = this.jumpForce;/*nastavi rychlost nahoru*/
+      this.onGround = false;/*hrac neni za zemi*/
     }
   }
 
   update() {
-    this.vy += this.gravity;
-    this.y += this.vy;
+    this.vy += this.gravity;/*Přidá gravitaci k rychlosti*/
+    this.y += this.vy;/*Posune hráče nahoru nebo dolů.*/
 
   
-    if (this.y > this.groundY) {
-      this.y = this.groundY;
-      this.vy = 0;
-      this.onGround = true;
+    if (this.y > this.groundY) { /*jestli hráč spadl pod zem*/
+      this.y = this.groundY; /*Vrátí hráče přesně na zem*/
+      this.vy = 0; /*nastavi */
+      this.onGround = true; /*Řekne, že hráč zase stojí na zemi*/
     }
   }
 
   draw() {
-    fill(0, 255, 0);
-    rectMode(CENTER);
-    rect(this.x, this.y, this.size, this.size);
+    fill(0, 255, 0); /*barva na zelenou barvu*/
+    rectMode(CENTER);/*Obdélník se kreslí od středu.*/
+    rect(this.x, this.y, this.size, this.size); /*Nakreslí hráče jako čtverec.*/
   }
 }
+
 
